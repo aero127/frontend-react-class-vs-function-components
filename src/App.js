f8e9a79fd6b040d69c7b3d4646c85f9e5e08ae84
aero127 <arnoldvdijk@hotmail.com>
2021-05-23 20:24:01 +0200

    opdracht klaar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,53 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [isLightOn, setLightOn] = React.useState('');
+
+    function handleClick(e) {
+        setLightOn(!isLightOn);
+    }
   return (
     <div>
-      Schrijf hier jouw functie-component!
+        <button
+            type="button"
+            onClick={(e) => handleClick(e)}
+            value={isLightOn}
+        >{isLightOn ? 'AAN' : 'UIT'}
+        </button>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLightOn: true
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            isLightOn: !this.state.isLightOn,
+        });
+    }
+
+    render() {
+        return (
+            <button
+                type="button"
+                onClick={this.handleClick}
+            >
+                {this.state.isLightOn ? 'AAN' : 'UIT'}
+            </button>
+        );
+    }
+}
+
+export default App;*/
